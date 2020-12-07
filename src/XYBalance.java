5c249f7a7c6fc2d2a20c76565c789e52d7c50628
Jan Gevaert <jan.gevaert@gmail.com>
2020-12-06 17:56:31 -0700

    Initial commit

---
+++ src/XYBalance.java
@@ -0,0 +1,14 @@
+public class XYBalance {
+/*
+
+We'll say that a String is xy-balanced if for all the 'x' chars in the string,
+there exists a 'y' char somewhere later in the string.
+So "xxy" is balanced, but "xyx" is not.
+One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.
+ */
+
+  public boolean xyBalance(String str) {
+
+    return (str.indexOf('x') == -1 || str.lastIndexOf('x') < str.lastIndexOf('y'));
+  }
+}
