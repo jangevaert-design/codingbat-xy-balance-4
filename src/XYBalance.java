f1352923853d96055fb3adbe0d1c7c167fc997e4
Jan Gevaert <jan.gevaert@gmail.com>
2020-12-06 17:59:40 -0700

    Added explanation.

--- src/XYBalance.java
+++ src/XYBalance.java
@@ -7,6 +7,7 @@ So "xxy" is balanced, but "xyx" is not.
 One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.
  */
 
+  //str.indexOf('x') == -1 means that there is no x in the String.
   public boolean xyBalance(String str) {
 
     return (str.indexOf('x') == -1 || str.lastIndexOf('x') < str.lastIndexOf('y'));
